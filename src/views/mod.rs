44bf41fab6e1539dfff5c89914fe7e8bab484eed
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-08-13 19:53:18 +0430

    Attach 'create' view to the app

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,8 +1,10 @@
 use actix_web::web::ServiceConfig;
 
 mod auth;
+mod item;
 mod path;
 
 pub fn views_factory(app: &mut ServiceConfig) {
     auth::auth_factory(app);
+    item::item_factory(app);
 }
