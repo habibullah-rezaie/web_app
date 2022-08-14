6303ce544e97747a4dc398e2c253b54a8acc782f
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-08-14 06:55:17 +0430

    Attach the 'get' view to the app

--- src/views/item/mod.rs
+++ src/views/item/mod.rs
@@ -2,9 +2,11 @@ use actix_web::web::{self, ServiceConfig};
 
 use crate::views::path::Path;
 
-use self::create::create;
+use self::{create::create, get::get};
 
 mod create;
+mod get;
+mod utils;
 
 pub fn item_factory(app: &mut ServiceConfig) {
     let item_path = Path {
@@ -15,4 +17,6 @@ pub fn item_factory(app: &mut ServiceConfig) {
         &item_path.define("/create/{title}".to_string()),
         web::get().to(create),
     );
+
+    app.route(&item_path.define("/get".to_string()), web::get().to(get));
 }
