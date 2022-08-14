f25a1d85acffd1936825354ad32bfa7fd7b4d60e
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-08-14 07:16:28 +0430

    Don't allow rewrite to file

--- src/to_do/structs/traits/create.rs
+++ src/to_do/structs/traits/create.rs
@@ -6,8 +6,7 @@ pub trait Create {
     fn create(&self, title: &str, status: &str, state: &mut Map<String, Value>) {
         match state.insert(title.to_string(), json!(status.to_string())) {
             Some(_) => {
-                println!("\nThis todo already exists. Aborting.");
-                return;
+                panic!("\nThis todo already exists. Aborting.");
             }
             None => {}
         }
