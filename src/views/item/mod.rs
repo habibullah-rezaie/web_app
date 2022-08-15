d5a658d2846132a3564bae0293e605b6b2c94708
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-08-15 20:31:53 +0430

    Attach newly created views to app

--- src/views/item/mod.rs
+++ src/views/item/mod.rs
@@ -2,9 +2,11 @@ use actix_web::web::{self, ServiceConfig};
 
 use crate::views::path::Path;
 
-use self::{create::create, get::get};
+use self::{create::create, delete::delete, edit::edit, get::get};
 
 mod create;
+mod delete;
+mod edit;
 mod get;
 mod utils;
 
@@ -18,5 +20,11 @@ pub fn item_factory(app: &mut ServiceConfig) {
         web::post().to(create),
     );
 
+    app.route(
+        &item_path.define("/delete/{title}".to_string()),
+        web::delete().to(delete),
+    );
+
     app.route(&item_path.define("/get".to_string()), web::get().to(get));
+    app.route(&item_path.define("/edit".to_string()), web::put().to(edit));
 }
